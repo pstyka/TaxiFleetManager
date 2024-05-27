6392046baef703aedde861c7b52048e6422cc927
Pawel Styka <pawel@styka.pl>
2024-05-27 11:53:28 +0200

    added angular submodule

--- backend/src/main/java/com/example/pawel/entities/Role.java
+++
@@ -1,6 +0,0 @@
-package com.example.pawel.entities;
-
-public enum Role {
-    DRIVER,
-    ADMIN
-}
--- backend/src/main/java/com/example/pawel/repository/AvailabilityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.pawel.repository;
-
-import com.example.pawel.entities.Availability;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface AvailabilityRepository extends JpaRepository<Availability, UUID>{
-}
